ad9ce47b7ea577fd898a337bde48595735511f9e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-09 10:33:02 -0300

    :package: Pronto - busca em Excel, SMS real do resultado

--- hashtag-python1aula/main.py
+++ hashtag-python1aula/main.py
@@ -7,8 +7,14 @@
 # pip install --upgrade pip
 
 import pandas as pd
+from twilio.rest import Client
 
-# Passo a passo de Solução 
+# Your Account SID from twilio.com/console
+account_sid = "AC620e412b2361fe740a3d7a7c6774b700"
+# Your Auth Token from twilio.com/console
+auth_token = "fee7e510b15f23c68ba40c2612a87698"
+client = Client(account_sid, auth_token) #conecta
+
 
 # Abrir os 6 arquivos excel
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
@@ -20,9 +26,14 @@ for mes in lista_meses:
     if (tabela_vendas['Vendas'] > 55000).any():
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
-        print(f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
-
-
+        #print(f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}.')
+        sms = f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}.'
+        message = client.messages.create(
+            to="+5561983737860",
+            from_="+12156081242",
+            body=sms
+        )
+        print(message.sid)
 
 #  Para cada arquivo:
 #     verificar se algum valor na coluna vendas ddaquele arquivo é maior que 55.000
@@ -30,3 +41,7 @@ for mes in lista_meses:
 # Se for maior que 55.000 -> enviar SMS com o nome, o mês e as vendas do vendedor
 
 # Caso não seja maior que 55.000 não fará nada...
+
+
+
+
